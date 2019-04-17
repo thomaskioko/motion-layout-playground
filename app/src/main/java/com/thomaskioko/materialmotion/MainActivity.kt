44d15a665748682abba66aa1b0a248d3b298ad2f
kioko <kiokotomas@gmail.com>
2019-04-17 21:43:37 +0200

    update file to load demos

--- app/src/main/java/com/thomaskioko/materialmotion/MainActivity.kt
+++ app/src/main/java/com/thomaskioko/materialmotion/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.thomaskioko.materialmotion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.thomaskioko.materialmotion.adapter.TweetsAdapter
+import com.thomaskioko.materialmotion.adapter.ListAdapter
+import com.thomaskioko.materialmotion.model.ExampleTypes
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler_view_tweets.apply {
-            adapter = TweetsAdapter()
+        recyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ListAdapter(mAdapterData)
+            setHasFixedSize(true)
         }
+    }
+
+    fun start(activity: Class<*>, layoutFileId: Int, types: ExampleTypes?) {
+        val intent = Intent(this, activity).apply {
+            putExtra("layoutId", layoutFileId)
+            putExtra("type", types?.ordinal ?: ExampleTypes.MOVIE_EPISODES.ordinal)
+        }
+        startActivity(intent)
     }
 }
