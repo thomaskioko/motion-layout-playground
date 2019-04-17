5e667652a4d45837572f520a1e31ef7efca91ed7
kioko <kiokotomas@gmail.com>
2019-04-17 21:24:48 +0200

    add description text

--- app/src/main/java/com/thomaskioko/materialmotion/DataDemo.kt
+++ app/src/main/java/com/thomaskioko/materialmotion/DataDemo.kt
@@ -4,6 +4,7 @@ import com.thomaskioko.materialmotion.model.ExampleTypes
 
 data class DemoData(
     val title: String,
+    val description: String,
     val type: ExampleTypes,
     val layout: Int = 0,
     val activity: Class<*> = DemoActivity::class.java
