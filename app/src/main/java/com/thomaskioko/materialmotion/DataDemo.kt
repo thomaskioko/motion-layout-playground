20c2109766cee54fb4467119cef13b5ca19610be
kioko <kiokotomas@gmail.com>
2019-04-17 19:04:36 +0200

    cleanup

--- app/src/main/java/com/thomaskioko/materialmotion/DataDemo.kt
+++ app/src/main/java/com/thomaskioko/materialmotion/DataDemo.kt
@@ -1,5 +1,10 @@
 package com.thomaskioko.materialmotion
 
-data class DemoData(val title: String, val type: ExampleTypes, val layout: Int = 0, val activity: Class<*> = DemoActivity::class.java) {
-    constructor(title: String, type: ExampleTypes, activity: Class<*> = DemoActivity::class.java) : this(title, type, 0, activity)
-}
+import com.thomaskioko.materialmotion.model.ExampleTypes
+
+data class DemoData(
+    val title: String,
+    val type: ExampleTypes,
+    val layout: Int = 0,
+    val activity: Class<*> = DemoActivity::class.java
+)
